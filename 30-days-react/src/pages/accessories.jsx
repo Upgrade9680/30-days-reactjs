dd6a0ba077d0fa03064ad40d91cd49b5c89e4b20
Upgrade9680 <satashish5@gmail.com>
2025-07-12 16:05:06 +0530

    add new page Support & Accessories

--- 30-days-react/src/pages/accessories.jsx
+++ 30-days-react/src/pages/accessories.jsx
@@ -6,32 +6,27 @@ const Accessories = () => {
     <>
     <div className='flex space-x-18 mt-10 ml-32 bg-white '> 
         <div>
-            <div className='text-[#818185] text-xs'>Shop</div>
-            <div className='text-2xl font-serif mt-3'>Shop the Latest</div>
+            <div className='text-[#818185] text-xs'>Shop Accessories</div>
+            <div className='text-2xl font-serif mt-3'>Shop All Accessories</div>
             <div className='text-2xl font-serif mt-2'>Mac</div>
             <div className='text-2xl font-serif mt-2'>iPad</div>
             <div className='text-2xl font-serif mt-2'>iPhone</div>
             <div className='text-2xl font-serif mt-2'>Apple Watch</div>
-            <div className='text-2xl font-serif mt-2'>Accessories</div>
-
+            <div className='text-2xl font-serif mt-2'>AirPods</div>
+            <div className='text-2xl font-serif mt-2'>TV& Home</div>
         </div>
 
         <div>
-            <div className='text-[#818185] text-xs'>Quick Links</div>
-            <div className='text-xs font-serif mt-3'>Find a Store</div>
-            <div className='text-xs font-serif mt-2'>Order Status</div>
-            <div className='text-xs font-serif mt-2'>Ways to Buy</div>
-            <div className='text-xs font-serif mt-2'>Personal Setup</div>
-            <div className='text-xs font-serif mt-2'>College Student Offer</div>
-           
-
+            <div className='text-[#818185] text-xs'>Explore Accessories</div>
+            <div className='text-xs font-serif mt-3'>Made by Apple</div>
+            <div className='text-xs font-serif mt-2'>Beat by Dr. Dre</div>
+            <div className='text-xs font-serif mt-2'>Air Tag</div>
         </div>
 
         <div>
-            <div className='text-[#818185] text-xs'>Shop Special Stores</div>
+            {/* <div className='text-[#818185] text-xs'>Shop Special Stores</div>
             <div className='text-xs font-serif mt-3'>Education</div>
-            <div className='text-xs font-serif mt-2'>Business</div>
-
+            <div className='text-xs font-serif mt-2'>Business</div> */}
         </div>
 
     </div>
--- 30-days-react/src/pages/support.jsx
+++ 30-days-react/src/pages/support.jsx
@@ -1,42 +1,40 @@
-
-import React from 'react'
+import React from "react";
 
 const Support = () => {
   return (
     <>
-    <div className='flex space-x-18 mt-10 ml-32 bg-white '> 
+      <div className="flex space-x-18 mt-10 ml-32 bg-white ">
         <div>
-            <div className='text-[#818185] text-xs'>Shop</div>
-            <div className='text-2xl font-serif mt-3'>Shop the Latest</div>
-            <div className='text-2xl font-serif mt-2'>Mac</div>
-            <div className='text-2xl font-serif mt-2'>iPad</div>
-            <div className='text-2xl font-serif mt-2'>iPhone</div>
-            <div className='text-2xl font-serif mt-2'>Apple Watch</div>
-            <div className='text-2xl font-serif mt-2'>Accessories</div>
-
+          <div className="text-[#818185] text-xs">Explore Support</div>
+          <div className="text-2xl font-serif mt-3">iPhone</div>
+          <div className="text-2xl font-serif mt-2">Mac</div>
+          <div className="text-2xl font-serif mt-2">iPad</div>
+          <div className="text-2xl font-serif mt-2">Watch</div>
+          <div className="text-2xl font-serif mt-2">AirPods</div>
+          <div className="text-2xl font-serif mt-2">Music</div>
+          <div className="text-2xl font-serif mt-2">TV</div>
+          <div className="text-1xl font-serif mt-5">Explore Support</div>
         </div>
 
         <div>
-            <div className='text-[#818185] text-xs'>Quick Links</div>
-            <div className='text-xs font-serif mt-3'>Find a Store</div>
-            <div className='text-xs font-serif mt-2'>Order Status</div>
-            <div className='text-xs font-serif mt-2'>Ways to Buy</div>
-            <div className='text-xs font-serif mt-2'>Personal Setup</div>
-            <div className='text-xs font-serif mt-2'>College Student Offer</div>
-           
-
+          <div className="text-[#818185] text-xs">Get Help</div>
+          <div className="text-xs font-serif mt-3">Community</div>
+          <div className="text-xs font-serif mt-2">Check Coverage</div>
+          <div className="text-xs font-serif mt-2">Repair</div>
         </div>
 
         <div>
-            <div className='text-[#818185] text-xs'>Shop Special Stores</div>
-            <div className='text-xs font-serif mt-3'>Education</div>
-            <div className='text-xs font-serif mt-2'>Business</div>
-
+          <div className="text-[#818185] text-xs">Helpful Topics</div>
+          <div className="text-xs font-serif mt-3">Get AppleCare+</div>
+          <div className="text-xs font-serif mt-2">
+            Apple Account & Password
+          </div>
+          <div className="text-xs font-serif mt-3">Billing & Subscriptions</div>
+          <div className="text-xs font-serif mt-2">Accessibility</div>
         </div>
-
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Support
+export default Support;
