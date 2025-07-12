e23501b713ac97fefa1f379beae98f1fb0945c34
Upgrade9680 <satashish5@gmail.com>
2025-07-12 14:46:22 +0530

    2nd days in react add pages

--- 30-days-react/src/pages/iPad.jsx
+++ 30-days-react/src/pages/iPad.jsx
@@ -1,42 +1,41 @@
-
-import React from 'react'
+import React from "react";
 
 const Ipad = () => {
   return (
     <>
-    <div className='flex space-x-18 mt-10 ml-32 bg-white '> 
+      <div className="flex space-x-18 mt-10 ml-32 bg-white ">
         <div>
-            <div className='text-[#818185] text-xs'>Shop</div>
-            <div className='text-2xl font-serif mt-3'>Shop the Latest</div>
-            <div className='text-2xl font-serif mt-2'>Mac</div>
-            <div className='text-2xl font-serif mt-2'>iPad</div>
-            <div className='text-2xl font-serif mt-2'>iPhone</div>
-            <div className='text-2xl font-serif mt-2'>Apple Watch</div>
-            <div className='text-2xl font-serif mt-2'>Accessories</div>
-
+          <div className="text-[#818185] text-xs">Explore iPad</div>
+          <div className="text-2xl font-serif mt-3">Explore All iPad</div>
+          <div className="text-2xl font-serif mt-2">iPad Pro</div>
+          <div className="text-2xl font-serif mt-2">iPad Air</div>
+          <div className="text-2xl font-serif mt-2">iPad mini</div>
+          <div className="text-2xl font-serif mt-2">Apple Pencil</div>
+          <div className="text-2xl font-serif mt-2">Keyboards</div>
+          <div className="text-1xl font-serif mt-5">Compare iPad</div>
+          <div className="text-1xl font-serif mt-2">Why iPad</div>
+        </div>
+        <div>
+          <div className="text-[#818185] text-xs">Shop iPad</div>
+          <div className="text-xs font-serif mt-3">Shop iPad</div>
+          <div className="text-xs font-serif mt-2">iPad Accessories</div>
+          <div className="text-xs font-serif mt-2">Ways to Buy</div>
+          <div className="text-xs font-serif mt-2">College Student Offer</div>
         </div>
 
         <div>
-            <div className='text-[#818185] text-xs'>Quick Links</div>
-            <div className='text-xs font-serif mt-3'>Find a Store</div>
-            <div className='text-xs font-serif mt-2'>Order Status</div>
-            <div className='text-xs font-serif mt-2'>Ways to Buy</div>
-            <div className='text-xs font-serif mt-2'>Personal Setup</div>
-            <div className='text-xs font-serif mt-2'>College Student Offer</div>
-           
-
+          <div className="text-[#818185] text-xs">More from iPad</div>
+          <div className="text-xs font-serif mt-3">iPad Support</div>
+          <div className="text-xs font-serif mt-2">AppleCare+ for iPad</div>
+          <div className="text-xs font-serif mt-3">iPadOS 26 Preview</div>
+          <div className="text-xs font-serif mt-2">Apple Intelligence</div>
+          <div className="text-xs font-serif mt-3">Apps by Apple</div>
+          <div className="text-xs font-serif mt-2">iCloud+</div>
+          <div className="text-xs font-serif mt-2">Education</div>
         </div>
-
-        <div>
-            <div className='text-[#818185] text-xs'>Shop Special Stores</div>
-            <div className='text-xs font-serif mt-3'>Education</div>
-            <div className='text-xs font-serif mt-2'>Business</div>
-
-        </div>
-
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Ipad;
