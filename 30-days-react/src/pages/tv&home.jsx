c5916d17e50f3cfb4714559bd1516d5eafef3f26
Upgrade9680 <satashish5@gmail.com>
2025-07-12 15:41:27 +0530

    Add page TV&Home

--- 30-days-react/src/pages/tv&home.jsx
+++ 30-days-react/src/pages/tv&home.jsx
@@ -1,42 +1,43 @@
-
-import React from 'react'
+import React from "react";
 
 const TVHome = () => {
   return (
     <>
-    <div className='flex space-x-18 mt-10 ml-32 bg-white '> 
+      <div className="flex space-x-18 mt-10 ml-32 bg-white ">
         <div>
-            <div className='text-[#818185] text-xs'>Shop</div>
-            <div className='text-2xl font-serif mt-3'>Shop the Latest</div>
-            <div className='text-2xl font-serif mt-2'>Mac</div>
-            <div className='text-2xl font-serif mt-2'>iPad</div>
-            <div className='text-2xl font-serif mt-2'>iPhone</div>
-            <div className='text-2xl font-serif mt-2'>Apple Watch</div>
-            <div className='text-2xl font-serif mt-2'>Accessories</div>
-
+          <div className="text-[#818185] text-xs">Explore TV & Home</div>
+          <div className="text-2xl font-serif mt-3">Explore TV & Home</div>
+          <div className="text-2xl font-serif mt-2">Apple TV 4K</div>
+          <div className="text-2xl font-serif mt-2">HomePod</div>
+          <div className="text-2xl font-serif mt-2">HomePod mini</div>
+          <div className="text-2xl font-serif mt-2">Apple Watch</div>
+          <div className="text-2xl font-serif mt-2">Accessories</div>
         </div>
 
         <div>
-            <div className='text-[#818185] text-xs'>Quick Links</div>
-            <div className='text-xs font-serif mt-3'>Find a Store</div>
-            <div className='text-xs font-serif mt-2'>Order Status</div>
-            <div className='text-xs font-serif mt-2'>Ways to Buy</div>
-            <div className='text-xs font-serif mt-2'>Personal Setup</div>
-            <div className='text-xs font-serif mt-2'>College Student Offer</div>
-           
-
+          <div className="text-[#818185] text-xs">Shop TV & Home</div>
+          <div className="text-xs font-serif mt-3">Shop apple TV 4k</div>
+          <div className="text-xs font-serif mt-2">Shop HomePod</div>
+          <div className="text-xs font-serif mt-2">Shop HomePod mini</div>
+          <div className="text-xs font-serif mt-2">Shop Siri Remote</div>
+          <div className="text-xs font-serif mt-2">TV & Home Accessories</div>
         </div>
 
         <div>
-            <div className='text-[#818185] text-xs'>Shop Special Stores</div>
-            <div className='text-xs font-serif mt-3'>Education</div>
-            <div className='text-xs font-serif mt-2'>Business</div>
-
+          <div className="text-[#818185] text-xs">More From TV & Apple</div>
+          <div className="text-xs font-serif mt-3">Apple Tv Support</div>
+          <div className="text-xs font-serif mt-2">HomePod Support</div>
+          <div className="text-xs font-serif mt-2">AppleCare+</div>
+          <div className="text-xs font-serif mt-2">Apple TV app</div>
+          <div className="text-xs font-serif mt-2">Apple TV+</div>
+          <div className="text-xs font-serif mt-3">Home App</div>
+          <div className="text-xs font-serif mt-2">Apple Music</div>
+          <div className="text-xs font-serif mt-2">Siri</div>
+          <div className="text-xs font-serif mt-2">AirPlay</div>
         </div>
-
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default TVHome
+export default TVHome;
